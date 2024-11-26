fc859ad606ed637f23080725a172c5d8b0f3ed4b
paul.maglay <paul.maglay@gmail.com>
2024-11-26 15:48:04 +0400

    Fix

---
+++ product.js
@@ -0,0 +1,54 @@
+let products = [
+    {
+        "id": 1001,
+        'title' : "Cat Food,25 lb bag",
+        'description' : "A 25 pound bag of <em>irresistible</em>,"
+        + "organic goodness for your cat.",
+        'price' : 2000,
+        'image': "images/cat.png",
+        'availableInventory':5,
+        'rating': 3,
+
+    },
+    {
+        'id': 1002,
+        'title' : "Title Product 2",
+        'description' : "Description2",
+        'price' : 3000,
+        'image': "images/cat2.png",
+        'availableInventory':7,
+        'rating': 3,
+
+    },
+    {
+        'id': 1003,
+        'title' : "Title Product 3",
+        'description' : "Description 4",
+        'price' : 4000,
+        'image': "images/cat3.png",
+        'availableInventory':9,
+        'rating': 3,
+
+    },
+    {
+        'id': 1004,
+        'title' : "Title Product 4",
+        'description' : "Description 4",
+        'price' : 5000,
+        'image': "images/cat4.png",
+        'availableInventory':8,
+        'rating': 3,
+
+    },
+    {
+        'id': 1005,
+        'title' : "Title Product 5",
+        'description' : "Description 5",
+        'price' : 6000,
+        'image': "images/cat5.png",
+        'availableInventory':4,
+        'rating': 3,
+
+    },
+
+]
